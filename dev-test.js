537010dbe96fc9bf046308bff070ee105410d5d8
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-01 19:53:25 -0600

    Adding mining block function

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,11 @@
 const Block = require('./block');
 
-const block = new Block('foo_tmstp', 'bar_lahs', 'zoo_hs', 'baz_dt');
+// const block = new Block('foo_tmstp', 'bar_lahs', 'zoo_hs', 'baz_dt');
+const minedBlock = Block.mineBlock(Block.genesis(), 'my_first_mined');
+
 
 // experimenting 
 
-console.log(block.toString());
-console.log(Block.genesis().toString());
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
+console.log(minedBlock.toString());
