d1a4f2f3ebedbcf96d1cffd552da28bc06181d15
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 13:21:28 -0600

    AddBlock to the chain method

--- blockchain.js
+++ blockchain.js
@@ -6,10 +6,10 @@ class Blockchain {
   }
 
   addBlock(data) {
-    const lastBlock = this.chain[this.chain.length-1];
-    const block = Block.mineBlock(lastBlock, data);
+    // const lastBlock = this.chain[this.chain.length-1];
+    const block = Block.mineBlock(this.chain[this.chain.length-1], data);
     this.chain.push(block);
 
-    return block;
+    return block
   }
 }
