48de8cf4e5ce8cbc45f4589d803b5caa07841efd
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 17:35:05 -0600

    Updating paths

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,4 @@
-const Block = require('./block');
+const Block = require('./blockchain/block');
 
 // const block = new Block('foo_tmstp', 'bar_lahs', 'zoo_hs', 'baz_dt');
 const minedBlock = Block.mineBlock(Block.genesis(), 'my_first_mined');
