d5aa1bab5227462af51ee4504b67c311c401cf82
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 13:35:38 -0600

    Testing chain: addBlock method

--- blockchain.test.js
+++ blockchain.test.js
@@ -11,4 +11,11 @@ describe('Blockchain', () => {
   it('starts with genesis block', () => {
     expect(chaintst.chain[0]).toEqual(Block.genesis());
   });
+
+  it('adds a block to chain with given data', () => {
+    const data = 'new block into chain test';
+    chaintst.addBlock(data);
+
+    expect(chaintst.chain[chaintst.chain.length-1].data).toEqual(data);
+  });
 });
