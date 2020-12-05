07af93afada5458dc860c82338dccc43caa874eb
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 14:25:30 -0600

    Adding upcomming chain validation method

--- blockchain.js
+++ blockchain.js
@@ -12,6 +12,31 @@ class Blockchain {
 
     return block
   }
+
+  isValidChain(chain) {
+    // 1- Compare the genesis block of the upcomming chain with one in this Blockchain
+    // stringify the genesis objects to compare them 
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+    /* now it's verified let's compare the Bolck befor the last one in the upcomming
+     chain matches the lastBlock og this Blockchain */
+    for (let i=1; i<chain.length; i++ ) {
+      const block = chain[i];
+      const lastBlock = chain[i-1];
+
+      /* need to verify either the lastHashe-s match or generate a hash with
+       the same data of the upcoming chain and compare it with the lastHash of the upcaming chain new block*/
+
+      if (block.lastHash !== lastBlock.lastHash ||
+          block.hash !== Block.blockHash(block)) {
+        return false;
+      }
+    }
+
+    // returns trues if it passes the two validation conditions above
+
+    return true;
+  }
 }
 
 module.exports = Blockchain;
