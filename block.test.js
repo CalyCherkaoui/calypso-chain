9de7a5c094e7416a6374c3db95db08a19851c796
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 12:54:02 -0600

    Creating fixtures data

--- block.test.js
+++ block.test.js
@@ -1,1 +1,14 @@
 const Block = require('./block');
+
+describe('Block', () => {
+  let lastBlock, data, block;
+
+  beforeEach(() => {
+    data = 'my data test';
+    lastBlock = Block.genesis();
+    block = Block.mineBlock(lastBlock, data);
+  });
+
+
+  it('sets the `data` to match the input', () => {});
+});
