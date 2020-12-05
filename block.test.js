2052ba998d0a6dcbc7a539143ff6f67f2ca70741
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-05 12:58:17 -0600

    Unit test lastHash to match hash of the last block

--- block.test.js
+++ block.test.js
@@ -13,4 +13,8 @@ describe('Block', () => {
   it('sets the `data` to match the input', () => {
     expect(block.data).toEqual(data);
   });
+
+  it('sets the `lastHash` to match the hash of the last block', () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
 });
