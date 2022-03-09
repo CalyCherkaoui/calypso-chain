3de297596acd16730019e6687fbd5b8b74a7f289
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-03-09 15:29:49 -0600

    update

--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,5 @@
 import Block from './blockchain/block';
 
-// const block = new Block('foo_tmstp', 'bar_lahs', 'zoo_hs', 'baz_dt');
 const minedBlock = Block.mineBlock(Block.genesis(), 'my_first_mined');
 
 console.log(minedBlock.toString());
