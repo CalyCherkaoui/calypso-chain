3f55eab836bab95cd3d8c0a69ff17a30b74856d3
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-03-11 12:16:03 -0600

    update

--- blockchain/index.js
+++ blockchain/index.js
@@ -6,7 +6,6 @@ class Blockchain {
   }
 
   addBlock(data) {
-    // const lastBlock = this.chain[this.chain.length-1];
     const block = Block.mineBlock(this.chain[this.chain.length-1], data);
     this.chain.push(block);
 
